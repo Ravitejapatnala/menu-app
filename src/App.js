a45e3b904c08e3d0b0ceb36cb704e67c74d9147b
akhil <akhil.ce08@gmail.com>
2024-01-17 15:10:22 +0530

    first changes and uploaded files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import menu from "./Data/menu";
 
 function App() {
+  const[category, setCategory]= useState(menu)
+
+  function filterAllDishes(){
+    setCategory(menu);
+  }
+
+  function filterBreakfastDishes(){
+    setCategory(menu.filter((item)=>(
+      item.category=='breakfast'?[...category, {item}]: ""
+    )))
+  }
+
+  function filterLunchDishes(){
+    setCategory(menu.filter((item)=>(
+      item.category=='lunch' ? [...category, {item}]: ""
+    )))
+  }
+
+  function filterShakes(){
+    setCategory(menu.filter((item)=>(
+      item.category=='shakes'?[...category, {item}]: ""
+    )))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={filterAllDishes}>All</button>
+      <button onClick={filterBreakfastDishes}>Breakfast</button>
+      <button onClick={filterLunchDishes}>Lunch</button>
+      <button onClick={filterShakes}>Shakes</button>
+      
+      {
+        category && (
+          <div>
+            {
+              category.map((item)=>(
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.price}</p>
+                  <p>{item.desc}</p>
+                </div>
+              ))
+            }
+          </div>
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default App;
